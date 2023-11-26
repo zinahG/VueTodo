bf61e1e9760650b8ed23b1d7511a9d965eaee6eb
zinahG <137327233+zinahG@users.noreply.github.com>
2023-11-26 09:29:49 +0300

    feat: used JSONPlaceholder Api

--- src/stores/todoStore.ts
+++ src/stores/todoStore.ts
@@ -9,17 +9,72 @@ interface ITodo {
   done: boolean;
 }
 
+// export const useTodoStore = defineStore("todos", () => {
+//   const todos = ref<ITodo[]>([]);
+
+//   function addTodo(newTodo: ITodo) {
+//     todos.value = [...todos.value, newTodo];
+//   }
+
+//   function removeTodo(todoId: number) {
+//     todos.value = todos.value.filter((todo) => todo.id !== todoId);
+//   }
+
+//   const todosAsc = computed(() =>
+//     todos.value.slice().sort((a: ITodo, b: ITodo) => a.createdAt - b.createdAt)
+//   );
+
+//   onMounted(() => {
+//     todos.value = JSON.parse(localStorage.getItem("todos") || "[]");
+//   });
+
+//   watch(todos, (newVal) => {
+//     localStorage.setItem("todos", JSON.stringify(newVal));
+//   });
+
+//   return {
+//     addTodo,
+//     removeTodo,
+//     todosAsc,
+//   };
+// });
+
 export const useTodoStore = defineStore("todos", () => {
   const todos = ref<ITodo[]>([]);
+  const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
-  function addTodo(newTodo: ITodo) {
-    todos.value = [...todos.value, newTodo];
+  async function addTodo(newTodo: ITodo) {
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Failed to add todo");
+      }
+      const addedTodo = await response.json();
+      todos.value.push(addedTodo);
+    } catch (error) {
+      console.error("Error adding todo:", error);
+    }
   }
 
-  function removeTodo(todoId: number) {
-    todos.value = todos.value.filter((todo) => todo.id !== todoId);
+  async function removeTodo(todoId: number) {
+    try {
+      const response = await fetch(`${apiUrl}/${todoId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to remove todo");
+      }
+      todos.value = todos.value.filter((todo) => todo.id !== todoId);
+    } catch (error) {
+      console.error("Error removing todo:", error);
+    }
   }
-
   const todosAsc = computed(() =>
     todos.value.slice().sort((a: ITodo, b: ITodo) => a.createdAt - b.createdAt)
   );
